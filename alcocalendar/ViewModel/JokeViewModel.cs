97f7dbeb3419196a56886b15f6e601034e2b5034
scary101 <ScaRy101@yandex.ru>
2024-05-01 20:45:59 +0300

    ...

---
+++ alcocalendar/ViewModel/JokeViewModel.cs
@@ -0,0 +1,29 @@
+﻿using alcocalendar.View;
+using alcocalendar.ViewModel.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace alcocalendar.ViewModel
+{
+    internal class JokeViewModel
+    {
+        private readonly JokeWindow _jokeWindow;
+
+    public BindCommand CloseWin { get; set; }
+
+    public JokeViewModel(JokeWindow jokeWindow)
+    {
+        _jokeWindow = jokeWindow;
+        CloseWin = new BindCommand(_ => Closss());
+        }
+
+    public void Closss()
+    {
+        _jokeWindow.Close();
+    }
+    }
+}
